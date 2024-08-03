471c74787fe457bb4ae2aef89a490f3d96f8c302
Radu Marias <radumarias@gmail.com>
2024-08-03 21:57:17 +0300

    add bridge and example app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rencfs-kotlin
-Kotlin GUI for rencfs
+
+Kotlin GUI for rencfs.
+
+- [kotlin-bridge](kotlin-bridge) builds the `librust_jni.so` in [release](kotlin-bridge/target/release) dir. That folder you need to provide when running Kotlin app.
+- [kotlin](kotlin) is an example app that uses the bridge to call Rust code. It has an example for a normal flow and several simulations of errors and dry-run.
---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    application
+}
+
+group = "rencfs-kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("rencfs-kotlin"))
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt" // Replace with your main class
+    }
+    from(sourceSets.main.get().output)
+}

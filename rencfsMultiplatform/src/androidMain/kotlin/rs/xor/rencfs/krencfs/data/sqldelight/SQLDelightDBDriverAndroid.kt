02bf7eefee1eb094825c750758a55bd2553a9d44
Andrei Mărcuţ <andreimarcut@gmail.com>
2024-09-07 19:00:42 +0200

    per platform project isolates

---
+++ rencfsMultiplatform/src/androidMain/kotlin/rs/xor/rencfs/krencfs/data/sqldelight/SQLDelightDBDriverAndroid.kt
@@ -0,0 +1,21 @@
+package rs.xor.rencfs.krencfs.data.sqldelight
+
+import app.cash.sqldelight.async.coroutines.awaitCreate
+import app.cash.sqldelight.async.coroutines.synchronous
+import app.cash.sqldelight.db.QueryResult
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.db.SqlSchema
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import java.io.File
+
+actual suspend fun provideSQLDriver(
+    schema: SqlSchema<QueryResult.AsyncValue<Unit>>,
+    databaseName: String,
+    databaseFileName: String,
+): SqlDriver = AndroidSqliteDriver(schema.synchronous(), null!!, databaseFileName)
+        .apply {
+            println("provideSQLDriver: $this")
+            if (!File(databaseFileName).exists()) {
+                schema.awaitCreate(this)
+            }
+        }

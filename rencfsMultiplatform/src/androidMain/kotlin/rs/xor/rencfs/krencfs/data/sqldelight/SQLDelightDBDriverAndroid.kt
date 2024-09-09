b351f6d56af608ec5f72300524b02b597c92e1fe
Andrei Mărcuţ <andreimarcut@gmail.com>
2024-09-09 21:56:55 +0200

    cleanup

--- rencfsMultiplatform/src/androidMain/kotlin/rs/xor/rencfs/krencfs/data/sqldelight/SQLDelightDBDriverAndroid.kt
+++ rencfsMultiplatform/src/androidMain/kotlin/rs/xor/rencfs/krencfs/data/sqldelight/SQLDelightDBDriverAndroid.kt
@@ -1,16 +1,11 @@
 package rs.xor.rencfs.krencfs.data.sqldelight
 
-import android.content.Context
-import app.cash.sqldelight.async.coroutines.awaitCreate
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import rs.xor.rencfs.krencfs.RootContextProvider
-import java.io.File
-
-lateinit var context: Context
 
 actual suspend fun provideSQLDriver(
     schema: SqlSchema<QueryResult.AsyncValue<Unit>>,

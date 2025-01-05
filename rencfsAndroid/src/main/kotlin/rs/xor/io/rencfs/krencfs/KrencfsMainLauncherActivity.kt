f8abc77825ac96359fd1265673cfab33f588f293
Andrei Mărcuţ <andreimarcut@gmail.com>
2025-01-05 13:52:47 +0100

    Fix android build (#63)

--- rencfsAndroid/src/main/kotlin/rs/xor/io/rencfs/krencfs/KrencfsMainLauncherActivity.kt
+++ rencfsAndroid/src/main/kotlin/rs/xor/io/rencfs/krencfs/KrencfsMainLauncherActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import rs.xor.rencfs.krencfs.DeviceType
+import rs.xor.rencfs.krencfs.display.DisplayType
 import rs.xor.rencfs.krencfs.RencfsComposeAdaptiveApp
 import rs.xor.rencfs.krencfs.ui.design.RencfsMaterialDarkTheme
 
@@ -16,7 +16,7 @@ class KrencfsMainLauncherActivity : ComponentActivity() {
         Log.d("KrencfsMainLauncherActivity", "onCreate")
         setContent {
             RencfsMaterialDarkTheme {
-                RencfsComposeAdaptiveApp(DeviceType.Phone)
+                RencfsComposeAdaptiveApp(DisplayType.Phone)
             }
         }
     }

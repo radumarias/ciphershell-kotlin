b12f1fe312a3c0a50823728f22ae63cd0d48444d
Andrei Mărcuţ <andreimarcut@gmail.com>
2025-01-06 21:12:05 +0100

    Update WelcomeScreen.kt

--- rencfsMultiplatform/src/androidMain/preview/rs/xor/rencfs/krencfs/ui/screen/WelcomeScreen.kt
+++ rencfsMultiplatform/src/androidMain/preview/rs/xor/rencfs/krencfs/ui/screen/WelcomeScreen.kt
@@ -2,7 +2,7 @@ package rs.xor.rencfs.krencfs.ui.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import rs.xor.rencfs.krencfs.screen.WelcomeScreen
+//import rs.xor.rencfs.krencfs.screen.WelcomeScreen
 import rs.xor.rencfs.krencfs.ui.design.RencfsMaterialDarkTheme
 
 @Preview(
@@ -12,6 +12,6 @@ import rs.xor.rencfs.krencfs.ui.design.RencfsMaterialDarkTheme
 @Composable
 fun PreviewWelcomeScreen() {
     RencfsMaterialDarkTheme {
-        WelcomeScreen()
+     //   WelcomeScreen()
     }
 }

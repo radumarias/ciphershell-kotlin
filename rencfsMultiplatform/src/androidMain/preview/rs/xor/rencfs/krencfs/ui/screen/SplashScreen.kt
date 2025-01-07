c3f1ef5e27b38093bc13e28b384f9fc852fee80b
Andrei Mărcuţ <andreimarcut@gmail.com>
2025-01-07 09:20:36 +0100

    fix build

--- rencfsMultiplatform/src/androidMain/preview/rs/xor/rencfs/krencfs/ui/screen/SplashScreen.kt
+++ rencfsMultiplatform/src/androidMain/preview/rs/xor/rencfs/krencfs/ui/screen/SplashScreen.kt
@@ -3,7 +3,6 @@ package rs.xor.rencfs.krencfs.ui.screen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import rs.xor.rencfs.krencfs.screen.SplashScreen
-import rs.xor.rencfs.krencfs.screen.WelcomeScreen
 import rs.xor.rencfs.krencfs.ui.design.RencfsMaterialDarkTheme
 
 @Preview(
